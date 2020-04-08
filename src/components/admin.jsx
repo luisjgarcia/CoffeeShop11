361e9b8734c0b71b25243be5b58e19757500326b
Jose Ramos <jmrp.04@gmail.com>
2020-04-08 17:23:39 -0400

    Working on the admin side. The information of the form is stored in a class.

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -1,6 +1,35 @@
 import React, { Component } from "react";
+// import Book from "/Users/jose/Documents/Full Stack Web Developer/CoffeeShop11/src/components/book";
+
+class Book {
+    
+  constructor(title, author,year,isbn,copies,available){
+      this.title = title;
+      this.autor = author;
+      this.year = year;
+      this.isbn = isbn;
+      this.copies = copies;
+      this.available = available;
+  }
+}
 
 class Admin extends Component {
+
+  state = {
+    //books: [],
+  };
+
+  onAdd = () => {
+    // alert(document.getElementById("book-title").value);
+    var book = new Book(document.getElementById("book-title").value, document.getElementById("book-author").value, document.getElementById("year-of-publication").value, document.getElementById("book-isbn").value, document.getElementById("book-copies").value, document.getElementById("book-copies").value);
+    alert(book.copies);
+  }
+
+  onCancel = () => {
+    document.getElementById("new-book-form").reset();
+    // document.getElementById("book-title").value = "klk";
+  }
+
   state = {};
   render() {
     return(
@@ -8,7 +37,7 @@ class Admin extends Component {
       <h1>Admin</h1>
       <div class="new-book-form">
         <h3>Add a book:</h3>
-        <form>
+        <form id="new-book-form">
           <div class="form-group row">
             <label for="book-title" class="col-sm-2 col-form-label">Title</label>
             <div class="col-10">
@@ -24,7 +53,7 @@ class Admin extends Component {
           <div class="form-group row">
             <label for="year-of-publication" class="col-sm-2 col-form-label">Year of Publication</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control col-6" id="year-of-publication"></input>
+              <input type="number" class="form-control col-2 col-lg-1 col-md-2 col-sm-2" id="year-of-publication"></input>
             </div>
           </div>
           <div class="form-group row">
@@ -36,12 +65,12 @@ class Admin extends Component {
           <div class="form-group row">
             <label for="book-copies" class="col-sm-2 col-form-label"># of copies</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control col-6" id="book-copies"></input>
+              <input type="number" class="form-control col-2 col-lg-1 col-md-2 col-sm-2" id="book-copies"></input>
             </div>
           </div>
         </form>
-        <button id="add-to-inventory-button" className="btn btn-success btn-md">Add to inventory</button>
-        <button id="cancel-button" className="btn btn-danger btn-md">Cancel</button>
+        <button id="add-to-inventory-button" className="btn btn-success btn-md" onClick={() => this.onAdd()}>Add to inventory</button>
+        <button id="cancel-button" className="btn btn-danger btn-md" onClick={() => this.onCancel()}>Cancel</button>
       </div>
     </div>
 
