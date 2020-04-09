9161fbb299ee378bf20201959b3ba9bc4fbda803
kimdolion <kimowilkes@gmail.com>
2020-04-09 11:30:52 -0400

    Update individual book to check out and return - Updated individual book page with checkout and return buttons - Updated to live update available/copies

--- src/components/book.jsx
+++ src/components/book.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import config from "../config.json";
+import { ToastContainer, toast } from "react-toastify";
 import http from "../services/httpService";
 
 class Book extends Component {
@@ -16,10 +17,35 @@ class Book extends Component {
     this.setState({ book });
   }
 
+  onCheckout = async (book) => {
+    if (book.available > 0) {
+      book.available -= 1;
+
+      // Update database
+      // send book.id + book
+      await http.put(config.apiEndpoint + "/" + book._id, book);
+      toast.success(book.title + " has been checked out");
+      this.setState({ book });
+    } else toast.error("Out Of Stock");
+  };
+
+  onReturnBook = async (book) => {
+    if (book.available < book.copies) {
+      book.available += 1;
+
+      // Update database
+      // send book.id + book
+      await http.put(config.apiEndpoint + "/" + book._id, book);
+      toast(book.title + " has been returned");
+      this.setState({ book });
+    }
+  };
+
   render() {
     const book = this.state.book;
     return (
       <React.Fragment>
+        <ToastContainer />
         <div className="bg-dark text-light container-fluid">
           <div className="row">
             <div className="col">
@@ -36,9 +62,21 @@ class Book extends Component {
               <img src={book.image_url_l} alt="book cover" />
             </div>
           </div>
-          <Link to="/Library" className="btn btn-secondary">
-            Back to Library
-          </Link>
+          <div className="container">
+            <div className="row">
+              <Link to="/Library" className="btn btn-secondary">
+                Back to Library
+              </Link>
+              <button
+                className="btn btn-success"
+                onClick={() => this.onCheckout(book)}>Check-out
+              </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => this.onReturnBook(book)}>Return
+              </button>
+            </div>
+          </div>
         </div>
       </React.Fragment>
     );
