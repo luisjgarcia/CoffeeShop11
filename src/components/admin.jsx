0f563efe1823b48e3e582c836c01f58711ac8f3d
kimdolion <kimowilkes@gmail.com>
2020-04-08 21:39:19 -0400

    Update class to className

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 // import Book from "/Users/jose/Documents/Full Stack Web Developer/CoffeeShop11/src/components/book";
 
 class Book {
-    
+
   constructor(title, author,year,isbn,copies,available){
       this.title = title;
       this.autor = author;
@@ -34,39 +34,39 @@ class Admin extends Component {
   state = {};
   render() {
     return(
-    <div class="admin-page" id="library-display">
+    <div className="admin-page" id="library-display">
       <h1>Admin</h1>
-      <div class="new-book-form">
+      <div className="new-book-form">
         <h3>Add a book:</h3>
         <form id="new-book-form">
-          <div class="form-group row">
-            <label for="book-title" class="col-sm-2 col-form-label">Title</label>
-            <div class="col-10">
-              <input type="text" class="form-control col-6" id="book-title"></input>
+          <div className="form-group row">
+            <label for="book-title" className="col-sm-2 col-form-label">Title</label>
+            <div className="col-10">
+              <input type="text" className="form-control col-6" id="book-title"></input>
             </div>
           </div>
-          <div class="form-group row">
-            <label for="book-author" class="col-sm-2 col-form-label">Author</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control col-6" id="book-author"></input>
+          <div className="form-group row">
+            <label for="book-author" className="col-sm-2 col-form-label">Author</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control col-6" id="book-author"></input>
             </div>
           </div>
-          <div class="form-group row">
-            <label for="year-of-publication" class="col-sm-2 col-form-label">Year of Publication</label>
-            <div class="col-sm-10">
-              <input type="number" class="form-control col-2 col-lg-1 col-md-2 col-sm-2" id="year-of-publication"></input>
+          <div className="form-group row">
+            <label for="year-of-publication" className="col-sm-2 col-form-label">Year of Publication</label>
+            <div className="col-sm-10">
+              <input type="number" className="form-control col-2 col-lg-1 col-md-2 col-sm-2" id="year-of-publication"></input>
             </div>
           </div>
-          <div class="form-group row">
-            <label for="book-isbn" class="col-sm-2 col-form-label">ISBN</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control col-6" id="book-isbn"></input>
+          <div className="form-group row">
+            <label for="book-isbn" className="col-sm-2 col-form-label">ISBN</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control col-6" id="book-isbn"></input>
             </div>
           </div>
-          <div class="form-group row">
-            <label for="book-copies" class="col-sm-2 col-form-label"># of copies</label>
-            <div class="col-sm-10">
-              <input type="number" class="form-control col-2 col-lg-1 col-md-2 col-sm-2" id="book-copies"></input>
+          <div className="form-group row">
+            <label for="book-copies" className="col-sm-2 col-form-label"># of copies</label>
+            <div className="col-sm-10">
+              <input type="number" className="form-control col-2 col-lg-1 col-md-2 col-sm-2" id="book-copies"></input>
             </div>
           </div>
         </form>
